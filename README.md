6c4f6dcd237dbb8b0d366051b6c68c105d422068
Qi Yan <qiyanthor@gmail.com>
2021-12-28 12:59:05 +0100

    sync

---
+++ README.md
@@ -0,0 +1,111 @@
+# CrossLoc Benchmark Datasets Setup
+
+This repository contains CrossLoc Benchmark Datasets setup and splitting scripts. Please make sure you have access to the **CrossLoc Benchmark Raw Datasets** before proceeding. 
+
+Ideally, you may want to use this repository as the starting template of your own project, as the python dependency, dataset and basic dataloader have already been developed. There is simply no reason to re-build the wheel. 
+
+Happy coding! :)
+
+
+
+The CrossLoc Benchmark datasets are officially presented in the paper
+<br>
+**CrossLoc: Scalable Aerial Localization Assisted by Multimodal Synthetic Data**
+<br>
+[Qi Yan](https://qiyan98.github.io/), [Jianhao Zheng](https://jianhao-zheng.github.io/), [Simon Reding](https://people.epfl.ch/simon.reding/?lang=en), [Shanci Li](https://people.epfl.ch/shanci.li/?lang=en), [Iordan Doytchinov](https://people.epfl.ch/iordan.doytchinov?lang=en) 
+<br>
+École Polytechnique Fédérale de Lausanne (EPFL)
+<br>
+Links: **[arXiv](https://arxiv.org/abs/2112.09081) | [code repos](https://github.com/TOPO-EPFL/CrossLoc)**
+
+
+## Install dependencies
+
+* If `conda` environment is available:
+
+```bash
+conda env create -f setup/environment.yml
+conda activate crossloc
+```
+
+* Otherwise, if `conda` environment is not readily available:
+
+```bash
+python3 -m venv venvcrossloc
+source venvcrossloc/bin/activate
+pip3 install pip -U && pip3 install -r setup/requirements.txt
+```
+
+`open3d==0.9.0` may raise an error at some environment. You may remove the version limit to proceed.
+
+## Setup datasets
+
+* Setup datasets: we adopt some [DSAC*](https://github.com/vislearn/dsacstar) resources and keep its dataset convention.
+
+```bash
+cd datasets
+echo $DATA_DIR # point to the CrossLoc Benchmark Raw Datasets
+
+export OUT_DIR=$(pwd)/urbanscape
+python setup_urbanscape.py --dataset_dir $DATA_DIR/urbanscape --output_dir $OUT_DIR
+
+export OUT_DIR=$(pwd)/naturescape
+python setup_naturescape.py --dataset_dir $DATA_DIR/naturescape --output_dir $OUT_DIR
+```
+
+Please note that all RGB images are linked to the `DATA_DIR` directory using symbolic link. 
+
+* When you have the raw dataset locally preserved, please **don't move it elsewhere after setting up**. Otherwise, the symbolic links to raw RGB images don't work.
+* Use `--ignore_3d_label` flag to omit 3D label generation such as coordinate, depth and normal, i.e., `python setup_urbanscape.py --dataset_dir $DATA_DIR --output_dir $OUT_DIR --ignore_3d_label`. In this case, only lightweight `calibration`, `poses`, `rgb`, and `semantics` folders will be generated (see next section for what they exactly mean).
+
+## Dataset structure
+
+  After setting up the dataset successfully, you would see such directory structure:
+
+```markdown
+├── test_drone_real               # drone-trajectory **in-place** real data, for testing
+│   ├── calibration                 # focal length
+│   ├── depth                       # (possibly) down-sampled z-buffer depth
+│   ├── init                        # (possibly) down-sampled scene coordiante
+│   ├── normal                      # (possibly) down-sampled surface normal
+│   ├── poses                       # 4x4 homogeneous cam-to-world transformation matrix
+│   ├── rgb                         # RGB image (symbolic link)
+│   └── semantics                   # (possibly) full-size semantics map
+├── test_drone_sim                # drone-trajectory **in-place** equivalent synthetic data, for testing
+├── test_oop_drone_real           # drone-trajectory **out-of-place** real data, for testing
+├── test_oop_drone_sim            # drone-trajectory **out-of-place** equivalent synthetic data, for testing
+├── train_drone_real              # drone-trajectory **in-place** real data, for training
+├── train_drone_sim               # drone-trajectory **in-place** equivalent synthetic data, for training
+├── train_oop_drone_real          # drone-trajectory **out-of-place** real data, for training
+├── train_oop_drone_sim           # drone-trajectory **out-of-place** equivalent synthetic data, for training
+├── train_sim                     # LHS synthetic data, for training
+├── train_sim_plus_drone_sim      # combination of LHS and drone-trajectory **in-place** synthetic data, for training
+├── train_sim_plus_oop_drone_sim  # combination of LHS and drone-trajectory **out-of-place** synthetic data, for training
+├── val_drone_real                # drone-trajectory **in-place** real data, for validation
+├── val_drone_sim                 # drone-trajectory **in-place** equivalent synthetic data, for validation
+├── val_oop_drone_real            # drone-trajectory **out-of-place** real data, for validation
+├── val_oop_drone_sim             # drone-trajectory **out-of-place** equivalent synthetic data, for validation
+└── val_sim                       # LHS synthetic data, for validation
+```
+
+All directories have the same sub-folders (`calibration`, `depth`, `init` and others). To make the folder tree concise, only the sub-folders in the very first directory `test_drone_sim` is shown.
+
+## Getting started
+
+See [`dataset_rollout.ipynb`](dataset_rollout.ipynb) to have a preview on the dataset!
+
+See [`dataset_statistics.ipynb`](dataset_statistics.ipynb) to compute some statistics of the dataset.
+
+# Citation
+
+If you find our code useful for your research, please cite the paper:
+
+````bibtex
+@article{yan2021crossloc,
+  title={CrossLoc: Scalable Aerial Localization Assisted by Multimodal Synthetic Data},
+  author={Yan, Qi and Zheng, Jianhao and Reding, Simon and Li, Shanci and Doytchinov, Iordan},
+  journal={arXiv preprint arXiv:2112.09081},
+  year={2021}
+}
+````
+
---
+++ datasets/setup_naturescape.py
@@ -0,0 +1,135 @@
+import argparse
+import random
+from setup_vnavdataset_utils import *
+
+
+def get_ecef_origin():
+    """Shift the origin to make the value of coordinates in ECEF smaller and increase training stability"""
+    # Warning: this is dataset specific!
+    ori_lon, ori_lat, ori_alt = 7.065, 46.3705, 1410
+    ori_x, ori_y, ori_z = pyproj.Transformer.from_crs("epsg:4979", "epsg:4978").transform(ori_lat, ori_lon, ori_alt)
+    print('Origin XYZ: {}, {}, {}'.format(ori_x, ori_y, ori_z))
+    origin = np.array([ori_x, ori_y, ori_z], dtype=np.float64)
+    return origin
+
+
+def main(args):
+    """Setup the dataset"""
+
+    print('===== Naturescape dataset setup starts =====')
+
+    origin = get_ecef_origin()
+
+    args.lhs_dir = os.path.abspath(os.path.join(args.dataset_dir, args.lhs_dir))
+    args.matching_dir = os.path.abspath(os.path.join(args.dataset_dir, args.matching_dir))
+
+    # Check raw data folder paths
+    assert os.path.exists(args.lhs_dir)
+    assert os.path.exists(args.matching_dir)
+
+    dst_dir = os.path.abspath(args.output_dir)
+    mkdir(dst_dir)
+
+    # Get matching data directories
+    drone_data_cesium_ls, drone_data_real_ls = [], []
+    oop_drone_data_cesium_ls, oop_drone_data_real_ls = [], []  # out of place data
+    winter_drone_data_cesium_ls, winter_drone_data_real_ls = [], []  # winter data
+    for root, dirs, files in os.walk(args.matching_dir):
+        if len(files):
+            if os.path.basename(root).endswith('-sim'):
+                root_real = os.path.join(os.path.dirname(root), os.path.basename(root).replace('-sim', '-real'))
+                assert os.path.exists(root_real)
+                if 'outofplace' in os.path.basename(root):
+                    oop_drone_data_cesium_ls.append(root)
+                    oop_drone_data_real_ls.append(root_real)
+                elif 'winter' in os.path.basename(root):
+                    winter_drone_data_cesium_ls.append(root)
+                    winter_drone_data_real_ls.append(root_real)
+                else:
+                    drone_data_cesium_ls.append(root)
+                    drone_data_real_ls.append(root_real)
+
+    sort_idx = np.argsort(drone_data_cesium_ls)
+    drone_data_cesium_ls = np.take(drone_data_cesium_ls, sort_idx).tolist()
+    drone_data_real_ls = np.take(drone_data_real_ls, sort_idx).tolist()
+
+    # Setup dataset folder by folder
+    # lhs synthetic data
+    process_folder(args.lhs_dir, None, dst_dir, 'lhs_sim', origin, args.stride, args.ignore_3d_label,
+                   args.force_semantics_downsampling)
+
+    # drone real-synthetic matching data
+    for cesium_path, real_path in zip(drone_data_cesium_ls, drone_data_real_ls):
+        process_folder(cesium_path, None, dst_dir, 'drone_sim', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+        process_folder(cesium_path, real_path, dst_dir, 'drone_real', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+
+    for cesium_path, real_path in zip(oop_drone_data_cesium_ls, oop_drone_data_real_ls):
+        process_folder(cesium_path, None, dst_dir, 'oop_drone_sim', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+        process_folder(cesium_path, real_path, dst_dir, 'oop_drone_real', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+
+    for cesium_path, real_path in zip(winter_drone_data_cesium_ls, winter_drone_data_real_ls):
+        process_folder(cesium_path, None, dst_dir, 'winter_drone_sim', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+        process_folder(cesium_path, real_path, dst_dir, 'winter_drone_real', origin, args.stride, args.ignore_3d_label,
+                       args.force_semantics_downsampling)
+
+    # Split data into training/validation/testing
+    for mode in ['lhs_sim', 'drone_sim', 'drone_real', 'oop_drone_sim', 'oop_drone_real',
+                 'winter_drone_sim', 'winter_drone_real']:
+        print('\n===== Splitting data in {:s} mode ====='.format(mode))
+        split_data(os.path.join(dst_dir, mode), mode, 'naturescape', args.ignore_3d_label)
+        shutil.rmtree(os.path.join(dst_dir, mode))
+
+    # Construct virtual dataset pointer for ALL synthetic training data
+    src_dir_ls = [os.path.join(dst_dir, 'train_sim'), os.path.join(dst_dir, 'train_drone_sim')]
+    virtual_merge_sections(src_dir_ls, os.path.join(dst_dir, 'train_sim_plus_drone_sim'), args.ignore_3d_label)
+
+    src_dir_ls = [os.path.join(dst_dir, 'train_sim'), os.path.join(dst_dir, 'train_oop_drone_sim')]
+    virtual_merge_sections(src_dir_ls, os.path.join(dst_dir, 'train_sim_plus_oop_drone_sim'), args.ignore_3d_label)
+
+    print('===== Naturescape dataset setup is done =====')
+
+
+if __name__ == '__main__':
+
+    mp.set_start_method('spawn')
+
+    parser = argparse.ArgumentParser(
+        description='Setup CrossLoc Benchmark Naturescape dataset.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--dataset_dir', type=str, required=True,
+                        help='Dataset storing the structured datasets.')
+
+    parser.add_argument('--lhs_dir', type=str, default='naturescape-LHS',
+                        help='Source directory for synthetic LHS dataset (training set).')
+
+    parser.add_argument('--matching_dir', type=str,
+                        default='matching/',
+                        help='Source directory for real-synthetic matching dataset.')
+
+    parser.add_argument('--output_dir', type=str,
+                        default=os.path.abspath(os.path.join(os.path.dirname(__file__), 'naturescape')),
+                        help='Destination directory for the organized data set. Use DSAC* convention.')
+
+    parser.add_argument('--stride', type=int, default=8,
+                        help='Downsampling rate.')
+
+    parser.add_argument('--force_semantics_downsampling', default=False, action='store_true',
+                        help='Forcefully downsample semantic map. Please use carefully!')
+
+    parser.add_argument('--ignore_3d_label', action='store_true',
+                        help="Save RGB and semantic maps only. Ignore all 3D related labels.")
+
+    args = parser.parse_args()
+    print(args)
+
+    # fix random seed for reproducibility
+    np.random.seed(2021)
+    random.seed(2021)
+
+    main(args)
